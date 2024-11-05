c2bc1bfea0524de4b4b99cb82816e4e83666901e
Tkwong <108210425+tk-wong@users.noreply.github.com>
2024-11-06 00:32:45 +0800

    add ExternalAPI to simulate the process of calling the external APT for payment Add ExternalAPIFactory to create the ExternalAPI object OctopusAPI and OctopusAPIFactory are implementations of ExternalAPI and ExternalAPIFactory respectively, which is used for testing the idea
    Add unit test case for OctopusAPI and OctopusAPIFactory

---
+++ src/ExternalAPI/ExternalAPI.java
@@ -0,0 +1,9 @@
+package ExternalAPI;
+
+/**
+ * ExternalAPI interface
+ * It is used to simulate the payment process of the external API
+ */
+public interface ExternalAPI {
+    boolean doPayment(int price);
+}
---
+++ src/Test/TestExternalAPI/OctopusAPITest.java
@@ -0,0 +1,71 @@
+package TestExternalAPI;
+
+import ExternalAPI.ExternalAPI;
+import ExternalAPI.OctopusAPI;
+import ExternalAPI.OctopusAPIFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+/**
+ * OctopusAPITest class
+ * It is used to test the OctopusAPI class and OctopusAPIFactory class
+ */
+public class OctopusAPITest {
+    Random random;
+    OctopusAPIFactory octopusAPIFactory;
+    /**
+     * Set up the test environment
+     * Initialize the random object with seed 10 and OctopusAPIFactory object
+     */
+    @BeforeEach
+    public void setUp() {
+        random = new Random(10);
+        octopusAPIFactory = new OctopusAPIFactory();
+        
+    }
+    
+    /**
+     * Test the OctopusAPIFactory class
+     * Create an OctopusAPI object and check if it is an instance of OctopusAPI
+     */
+    @Test
+    public void testOctopusFactory() {
+        ExternalAPI octopusAPI = octopusAPIFactory.createExternalAPI();
+        Assertions.assertTrue(octopusAPI instanceof OctopusAPI);
+    }
+    
+    /**
+     * Test the OctopusAPIFactory class with random object
+     * Create an OctopusAPI object and check if it is an instance of OctopusAPI
+     */
+    @Test
+    public void testOctopusFactory_Random(){
+        ExternalAPI octopusAPI = octopusAPIFactory.createExternalAPI(random);
+        Assertions.assertTrue(octopusAPI instanceof OctopusAPI);
+    }
+    
+    /**
+     * Test the doPayment method in OctopusAPI class
+     * Create an OctopusAPI object and check if the doPayment method returns true
+     * By trial and error, the random number generated is 113, which is greater than or equal to the price 100
+     */
+    @Test
+    public void testDoPayment(){
+        ExternalAPI octopusAPI = octopusAPIFactory.createExternalAPI(random);
+        Assertions.assertTrue(octopusAPI.doPayment(100));// random.nextInt(0, 100 * 2) = 113
+    }
+    
+    /**
+     * Test the doPayment method in OctopusAPI class
+     * Create an OctopusAPI object and check if the doPayment method returns false
+     * By trial and error, the random number generated is 3, which is less than the price 15
+     */
+    @Test
+    public void testDoPayment_Fail(){
+        ExternalAPI octopusAPI = octopusAPIFactory.createExternalAPI(random);
+        Assertions.assertFalse(octopusAPI.doPayment(15));// random.nextInt(0, 15 * 2) = 3
+    }
+}
